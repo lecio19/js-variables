e9e30f5ac4aaf1dcd42f6c05ad1f0640e08c8998
lecio19 <alex.macri01@gmail.com>
2024-10-11 16:20:36 +0200

    secondo esercizio

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-const numOne = 8; // NON MODIFICARE QUESTA RIGA
-const numTwo = 16; // NON MODIFICARE QUESTA RIGA
-const numThree = 32; // NON MODIFICARE QUESTA RIGA
+// 1. Dichiarazione di variabili
+// Crea una variabile chiamata name e assegnale il tuo nome.
+// Stampa il valore di nome nella console.
+let name = 'Alessio'
 
-// Sostituisci NaN di seguito utilizzando
-// gli operatori numerici di Javascript
+// 2. Tipi primitivi
+// Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+// un numero, una stringa e un valore booleano.
+// Stampa tutti i valori nella console.
+let number = 25
+let text = undefined
+let bool = false
 
-// 1. Imposta questa variabile come somma di numOne e numTwo
-const numOnePlusNumTwo = numOne + numTwo;
-console.log(numOnePlusNumTwo); // Output: 24
+console.log(number,text,bool)
+// 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
+// e le variabili sopra in modo da stampare correttamente 
+// in console la stringa con il nome completo
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
+const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
+const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-// 2. Imposta questa variabile come prodotto di numThree e numTwo
-const numThreeTimesNumTwo = NaN;
-console.log(numThree * numTwo); // Output: 512
+const fullName = firstName+ ' '+ lastName;
+console.log(fullName); // Output: "Jane Smith"
 
-// 3. Imposta questa variabile come divisione di numThree per numOne
-const numThreeDividedByNumOne = NaN;
-console.log(numThree / numTwo); // Output: 4
-
-// 4. Imposta questa variabile come differenza tra numThree e numOne
-const numThreeMinusNumOne = NaN;
-console.log(numThree - numOne); // Output: 4
-
-// 5. Imposta questa variabile come somma di numOne, numTwo e numThree
-const sum = numOne + numTwo + numThree;
-console.log(sum); // Output: 56
-
-// 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
-const numBytes = (numOne + numTwo + numThree) / numOne;
-console.log(numBytes); // Output: 7
+// 4. Imposta questa variabile utilizzando una proprietà
+// sulla variabile alphabet per ottenere la sua lunghezza
+const numberOfLettersInAlphabet = alphabet.length;
+console.log(numberOfLettersInAlphabet); // Output: 26
